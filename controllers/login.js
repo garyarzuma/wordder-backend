89b0b01715256ae0fb51862e8d7ec095ea9267ac
garyarzumanyan <garyarzumanyan@gmail.com>
2022-03-06 22:36:13 -0800

    Added route for regular login

---
+++ controllers/login.js
@@ -0,0 +1,80 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client(process.env.CLIENT_ID)
+
+loginRouter.post('/wordderLogin', async (request, response) => {
+  const body = request.body
+  const user = await User.findOne({ email: body.email })
+
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(body.password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  } 
+
+  const userForToken = {
+    email: user.email,
+    id: user._id,
+  }
+
+   // token expires in 60*60 seconds, that is, in one hour
+   const token = jwt.sign(
+    userForToken, 
+    process.env.SECRET,
+    { expiresIn: 60*60 }
+  )
+
+  response
+    .status(200)
+    .send({ token, user: user })
+})
+
+loginRouter.post("/v1/auth/google", async (req, res, next) => {
+  console.log(req.body)
+  const {token}   = req.body
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.CLIENT_ID
+  });
+  const { given_name, family_name, name, email, picture } = ticket.getPayload(); 
+  const userExist = await User.findOne({ email: email })  
+
+  if (!userExist){
+      try{
+          const user = new User({
+              email: email,
+              name: name,
+              fname:given_name,
+              lname: family_name,
+              passwordHash: "none",
+              picURL: picture
+          })
+          const savedUser = await user.save()
+          res.status(201)
+          res.json(savedUser) 
+      }catch (error) {
+          next(error)
+      }
+  }
+  else{
+      const Existinguser = {
+          email: email,
+          name: name,
+          fname:given_name,
+          lname: family_name,
+          passwordHash: "none",
+          picURL: picture
+      }
+      const updatedUserInfo = await User.findByIdAndUpdate(userExist._id, Existinguser, { new: true })
+      res.json(updatedUserInfo)
+  }
+})
+
+module.exports = loginRouter
--- index.js
+++ index.js
@@ -1,38 +1,20 @@
 require('dotenv').config()
+const https = require('https');
+const fs = require('fs');
 const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
-const { OAuth2Client } = require('google-auth-library')
-const { config } = require('dotenv')
-const client = new OAuth2Client(process.env.CLIENT_ID)
 const mongoose = require('mongoose')
-const User = require('./models/user')
 const MONGODB_URI = process.env.MONGODB_URI
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
-
+const options = {
+    key: fs.readFileSync('./cert.key'),
+    cert: fs.readFileSync('./cert.crt'),
+  };
 
 app.use(express.json())
 app.use(middleware.requestLogger)
@@ -47,66 +29,12 @@ mongoose.connect(MONGODB_URI)
     logger.info('error connecting to MongoDB:', error.message)
   })
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-app.post("/api/v1/auth/google", async (req, res, next) => {
-    const {token}   = req.body
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: process.env.CLIENT_ID
-    });
-    const { given_name, family_name, name, email, picture } = ticket.getPayload(); 
-    const userExist = await User.findOne({ username: email })  
-
-    if (!userExist){
-        try{
-            const user = new User({
-                username: email,
-                name: name,
-                fname:given_name,
-                lname: family_name,
-                passwordHash: "none",
-                picURL: picture
-            })
-            const savedUser = await user.save()
-            res.status(201)
-            res.json(savedUser) 
-        }catch (error) {
-            next(error)
-        }
-    }
-    else{
-        const Existinguser = {
-            username: email,
-            name: name,
-            fname:given_name,
-            lname: family_name,
-            passwordHash: "none",
-            picURL: picture
-        }
-        const updatedUserInfo = await User.findByIdAndUpdate(userExist._id, Existinguser, { new: true })
-        res.json(updatedUserInfo)
-    }
-})
-
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 const PORT = 3001
-app.listen(PORT, () => {
+https.createServer(options, app).listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
